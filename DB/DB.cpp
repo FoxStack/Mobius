721014521b7bee5b62e0e9674cdebb1ae737c818
Raf <rafcin.s@gmail.com>
2021-07-28 15:31:48 -0700

    Comments DB/Vector (chore: comments) (fix: minor)

--- DB/DB.cpp
+++ DB/DB.cpp
@@ -7,6 +7,24 @@ DB::DB(std::string path) { PATH = path; }
 
 DB::~DB() {}
 
+/*
+ * This class is really simple, the JSON object is convenient and this class
+ * follows a similar structure to a JS reducer. In JS you can create an immutable object for example
+ * User: { authid: "...", cookies: "..." ...} and using the reducer hook you can modify the data elegantly.
+ *
+ * This class will probably be modified in the future to closely resemble the redux structure. Why? Idk I think the json class is cool
+ * and I want to makes some C++ tools for the future.
+ */
+
+/**
+ * @brief Write - Writes a single json object to the first line of a txt file. The json contains child objects and is stringified on insertion.
+ * @param object type json
+ */
+
+/*
+ * For this project once you read the JSON you can do anything you want with it, the idea is that you read it, you modify it and when you plan to save the object
+ * or close the program, you write the entire object to the txt file.
+ */
 void DB::write(nlohmann::json object) {
   try {
     std::ofstream file;
@@ -21,6 +39,10 @@ void DB::write(nlohmann::json object) {
   }
 }
 
+/**
+ * @brief Read - Reads the json from storage. The string literal is converted to an object.
+ * @return json
+ */
 nlohmann::json DB::read() {
   try {
     std::string data;
--- Vector/Iterator/iterator.h
+++ Vector/Iterator/iterator.h
@@ -1,51 +1,126 @@
 #include <iterator>
 template <typename T> class iterator {
+
+  /*
+   * [Iterator Requirements]
+   * https://eel.is/c++draft/iterator.requirements
+   *
+   * Forward It Ref - 23.3.4.11
+   * Input It Ref - 23.3.4.9
+   * Random Access It Ref - 23.3.4.13
+   */
+
 protected:
   T *m_ptr;
+
 public:
+  /*
+   * Pointer Arithmetic
+   * https://en.cppreference.com/w/cpp/language/operator_arithmetic#Additive_operators
+   *
+   * https://en.cppreference.com/w/cpp/named_req/RandomAccessIterator
+   * LR operator+(L, R)
+   * LR operator-(L, R)
+   * T* operator+(T*, std::ptrdiff_t)
+   * T* operator+(std::ptrdiff_t, T*)
+   * T* operator-(T*, std::ptrdiff_t)
+   * std::ptrdiff_t operator-(T*, T*)
+   */
+
+  // https://eel.is/c++draft/iterator.requirements#iterator.concept.random.access
   using iterator_category = std::random_access_iterator_tag;
   using value_type = T;
+  // https://en.cppreference.com/w/cpp/types/ptrdiff_t
   using difference_type = std::ptrdiff_t;
   using pointer = T *;
   using reference = T &;
 
-  iterator(T* ptr = nullptr){m_ptr = ptr;}
-  iterator(const iterator<T>& rawit) = default;
-  ~iterator(){}
+  iterator(T *ptr = nullptr) { m_ptr = ptr; }
+  iterator(const iterator<T> &rawit) = default;
+  ~iterator() {}
 
-  iterator<T>& operator=(const iterator<T>& rawit) = default;
-  iterator<T>& operator=(T* ptr){m_ptr = ptr;return (*this);}
+  iterator<T> &operator=(const iterator<T> &rawit) = default;
+  iterator<T> &operator=(T *ptr) {
+    m_ptr = ptr;
+    return (*this);
+  }
 
-  operator bool() const { if (m_ptr) return true; else return false; }
-  bool operator==(const iterator<T> &rawit) const { return (m_ptr == rawit.gcptr()); }
-  bool operator!=(const iterator<T> &rawit) const { return (m_ptr != rawit.gcptr()); }
+  operator bool() const {
+    if (m_ptr)
+      return true;
+    else
+      return false;
+  }
+  bool operator==(const iterator<T> &rawit) const {
+    return (m_ptr == rawit.gcptr());
+  }
+  bool operator!=(const iterator<T> &rawit) const {
+    return (m_ptr != rawit.gcptr());
+  }
 
+  /**
+   * @brief += operator
+   * @param movement
+   * @return
+   */
   iterator<T> &operator+=(const difference_type &movement) {
     m_ptr += movement;
     return (*this);
   }
+
+  /**
+   * @brief -= operator
+   * @param movement
+   * @return
+   */
   iterator<T> &operator-=(const difference_type &movement) {
     m_ptr -= movement;
     return (*this);
   }
+
+  /**
+   * @brief ++ operator prefix
+   * @return
+   */
   iterator<T> &operator++() {
     ++m_ptr;
     return (*this);
   }
+
+  /**
+   * @brief -- operator prefix
+   * @return
+   */
   iterator<T> &operator--() {
     --m_ptr;
     return (*this);
   }
+
+  /**
+   * @brief ++ operator postfix
+   * @return
+   */
   iterator<T> operator++(int) {
     auto temp(*this);
     ++m_ptr;
     return temp;
   }
+
+  /**
+   * @brief -- operator postfix
+   * @return
+   */
   iterator<T> operator--(int) {
     auto temp(*this);
     --m_ptr;
     return temp;
   }
+
+  /**
+   * @brief + operator
+   * @param movement
+   * @return
+   */
   iterator<T> operator+(const difference_type &movement) {
     auto oldPtr = m_ptr;
     m_ptr += movement;
@@ -53,6 +128,12 @@ public:
     m_ptr = oldPtr;
     return temp;
   }
+
+  /**
+   * @brief - operator
+   * @param movement
+   * @return
+   */
   iterator<T> operator-(const difference_type &movement) {
     auto oldPtr = m_ptr;
     m_ptr -= movement;
