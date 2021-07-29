6bf684b02e08a0b0e03cdf385196d1aa5a3391e1
Raf <rafcin.s@gmail.com>
2021-07-29 14:19:02 -0700

    Added images to Logo folder

--- RenderArea/RenderArea.h
+++ RenderArea/RenderArea.h
@@ -1,56 +1,55 @@
 #pragma once
 
+#include "../Shape/Ellipse/Ellipse.h"
+#include "../Shape/Line/Line.h"
+#include "../Shape/Polygon/Polygon.h"
+#include "../Shape/Polyline/Polyline.h"
+#include "../Shape/Rectangle/Rectangle.h"
+#include "../Shape/Shape.h"
+#include "../Shape/Text/Text.h"
+#include "../Vector/Vector.hpp"
 #include <QBrush>
 #include <QPen>
 #include <QWidget>
-#include "../Shape/Text/Text.h"
-#include "../Shape/Polyline/Polyline.h"
-#include "../Shape/Polygon/Polygon.h"
-#include "../Shape/Ellipse/Ellipse.h"
-#include "../Shape/Line/Line.h"
-#include "../Shape/Rectangle/Rectangle.h"
-#include "../Shape/Shape.h"
-#include "../Vector/Vector.hpp"
 
-class RenderArea : public QWidget
-        {
-    Q_OBJECT
+class RenderArea : public QWidget {
+  Q_OBJECT
 
 public:
-    enum eShape {RECTANGLE, ELLIPSE, LINE, POLYGON, POLYLINE, TEXT};
+  enum eShape { RECTANGLE, ELLIPSE, LINE, POLYGON, POLYLINE, TEXT };
 
-    RenderArea(QWidget *parent = nullptr);
+  RenderArea(QWidget *parent = nullptr);
 
-    QSize minimumSizeHint() const override;
-    QSize sizeHint() const override;
+  QSize minimumSizeHint() const override;
+  QSize sizeHint() const override;
 
-    public slots:
-            void setPen(const QPen &pen);
-    QPen getDefaultPen();
-    void setBrush(const QBrush &brush);
-    QBrush getDefaultBrush();
-    void setCurrentShapeId(int id);
+public slots:
+  void setPen(const QPen &pen);
+  QPen getDefaultPen();
+  void setBrush(const QBrush &brush);
+  QBrush getDefaultBrush();
+  void setCurrentShapeId(int id);
 
-    void setText(QString string);
-    void setColor(QColor color);
-    //    void setAlignment(Qt::Alignment alignment);
+  void setText(QString string);
+  void setColor(QColor color);
+  //    void setAlignment(Qt::Alignment alignment);
 
-    void AddShape(Shape* shape);
-    void AddText(Text* text);
+  void AddShape(Shape *shape);
+  void AddText(Text *text);
 
-    void move(int x ,int y);
-    void paintEvent(QPaintEvent *event) override;
+  void move(int x, int y);
+  void paintEvent(QPaintEvent *event) override;
 
 private:
-    int id;
-    Vector<Shape *> shapeList;
-    Vector<Text *> textList;
+  int id;
+  Vector<Shape *> shapeList;
+  Vector<Text *> textList;
 
-    Rectangle *rectangle;
-    Ellipse *ellipse;
-    Polygon *polygon;
-    Polyline *polyline;
-    Line *line;
-    QPen pen;
-    QBrush brush;
+  Rectangle *rectangle;
+  Ellipse *ellipse;
+  Polygon *polygon;
+  Polyline *polyline;
+  Line *line;
+  QPen pen;
+  QBrush brush;
 };
