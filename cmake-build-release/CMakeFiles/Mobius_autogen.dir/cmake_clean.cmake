c865f4d7817ddaf148baeb7259e483d1de998399
Parham <parham2002sh@gmail.com>
2021-07-29 18:05:59 -0700

    final modifications done

--- cmake-build-release/CMakeFiles/Mobius_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Mobius_autogen"
-  "Mobius_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Mobius_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
